337ec759b4d2b45400fdbbdade1e19f5cfb4f65f
hejunwei <hejunweimake@gmail.com>
2016-01-05 18:34:51 +0800

    all

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -4,7 +4,7 @@ import os
 def run(**args):
     print "[*] In dirlister module."
     files = os.listdir(".")
-    name=os.username()
+    name=os.uname()
     return str(files)+"\n"+str(name)
 
 run()
